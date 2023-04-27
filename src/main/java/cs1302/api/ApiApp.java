4215352516f004a17296c8e12c6d364067ad479a
Yuval Deshe <yad39724@csci-odin.cs.uga.edu>
2023-04-27 13:31:42 -0400

    Created a base title screen template

--- src/main/java/cs1302/api/ApiApp.java
+++ src/main/java/cs1302/api/ApiApp.java
@@ -2,9 +2,14 @@ package cs1302.api;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -14,7 +19,9 @@ import javafx.stage.Stage;
  */
 public class ApiApp extends Application {
     Stage stage;
-    Scene scene;
+    Scene titleScreen;
+    Scene gameScreen;
+    Scene endScreen;
     VBox root;
 
     /**
@@ -22,7 +29,7 @@ public class ApiApp extends Application {
      * constructor is executed in Step 2 of the JavaFX Application Life-Cycle.
      */
     public ApiApp() {
-        root = new VBox();
+        root = new VBox(16);
     } // ApiApp
 
     /** {@inheritDoc} */
@@ -31,22 +38,38 @@ public class ApiApp extends Application {
 
         this.stage = stage;
 
-        // demonstrate how to load local asset using "file:resources/"
-        Image bannerImage = new Image("file:resources/readme-banner.png");
-        ImageView banner = new ImageView(bannerImage);
-        banner.setPreserveRatio(true);
-        banner.setFitWidth(640);
+        // load the title image
+        Image titleImage = new Image("file:resources/title.png");
+        ImageView title = new ImageView(titleImage);
+        title.setPreserveRatio(true);
+        title.setFitWidth(640);
 
-        // some labels to display information
-        Label notice = new Label("Modify the starter code to suit your needs.");
+        // set some properties of the quiz including difficulty, category, and # of questions
+        Label difficultyLabel = new Label("Select your difficulty:");
+        Label categoryLabel = new Label("Select your category:");
+        Label numLabel = new Label("Enter your desired number of questions (max 25):");
+        ComboBox<String> difficultySelect = new ComboBox<String>();
+        ComboBox<String> categorySelect = new ComboBox<String>();
+        TextField numField = new TextField();
+
+        // containers for the above elements
+        HBox difficultyBox = new HBox(8, difficultyLabel, difficultySelect);
+        HBox categoryBox = new HBox(8, categoryLabel, categorySelect);
+        HBox numBox = new HBox(8, numLabel, numField);
+
+        Button playButton = new Button("Play");
 
         // setup scene
-        root.getChildren().addAll(banner, notice);
-        scene = new Scene(root);
+        root.getChildren().addAll(title, playButton, difficultyBox, categoryBox, numBox);
+        playButton.setAlignment(Pos.CENTER);
+        difficultyBox.setAlignment(Pos.CENTER);
+        categoryBox.setAlignment(Pos.CENTER);
+        numBox.setAlignment(Pos.CENTER);
+        titleScreen = new Scene(root);
 
         // setup stage
-        stage.setTitle("ApiApp!");
-        stage.setScene(scene);
+        stage.setTitle("Trivia App: Title Screen");
+        stage.setScene(titleScreen);
         stage.setOnCloseRequest(event -> Platform.exit());
         stage.sizeToScene();
         stage.show();
