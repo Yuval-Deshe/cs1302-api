beb9e1989baab1810493081275a494dd68004dc8
Yuval Deshe <yad39724@csci-odin.cs.uga.edu>
2023-04-27 17:57:08 -0400

    Cleaned up the title screen and put the code to intialize it in its own method. Created a method to change which screen is shown.

--- src/main/java/cs1302/api/ApiApp.java
+++ src/main/java/cs1302/api/ApiApp.java
@@ -3,14 +3,11 @@ package cs1302.api;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.geometry.Pos;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -22,14 +19,16 @@ public class ApiApp extends Application {
     Scene titleScreen;
     Scene gameScreen;
     Scene endScreen;
-    VBox root;
+    VBox titleRoot;
+    VBox gameRoot;
+    VBox endRoot;
 
     /**
      * Constructs an {@code ApiApp} object. This default (i.e., no argument)
      * constructor is executed in Step 2 of the JavaFX Application Life-Cycle.
      */
     public ApiApp() {
-        root = new VBox(16);
+        titleRoot = new VBox(16);
     } // ApiApp
 
     /** {@inheritDoc} */
@@ -38,6 +37,32 @@ public class ApiApp extends Application {
 
         this.stage = stage;
 
+        createTitleScreen();
+
+        //gameScreen = new Scene(gameRoot, 800, 320);
+        //endScreen = new Scene(endRoot, 800, 320);
+
+        // setup stage
+        changeScreen(titleScreen, "Trivia App: Title Screen");
+        stage.setOnCloseRequest(event -> Platform.exit());
+        stage.sizeToScene();
+        stage.show();
+
+    } // start
+
+    /**
+     * Changes which scene is shown and updates the title of the window.
+     *
+     * @param scene the scene to change to
+     * @param title the new title of the window
+     */
+    private void changeScreen(Scene scene, String title) {
+        stage.setScene(scene);
+        stage.setTitle(title);
+    } // changeScreen
+
+    /** Initializes the titleScreen scene, to be shown when the app is launched.  */
+    private void createTitleScreen() {
         // load the title image
         Image titleImage = new Image("file:resources/title.png");
         ImageView title = new ImageView(titleImage);
@@ -49,31 +74,53 @@ public class ApiApp extends Application {
         Label categoryLabel = new Label("Select your category:");
         Label numLabel = new Label("Enter your desired number of questions (max 25):");
         ComboBox<String> difficultySelect = new ComboBox<String>();
+        difficultySelect.getItems().addAll("Easy", "Medium", "Hard");
         ComboBox<String> categorySelect = new ComboBox<String>();
+        categorySelect.getItems().addAll(
+            "Any Category",
+            "General Knowledge",
+            "Books",
+            "Film",
+            "Music",
+            "Musicals & Theatre",
+            "Television",
+            "Video Games",
+            "Board Games",
+            "Science & Nature",
+            "Computers",
+            "Mathematics",
+            "Mythology",
+            "Sports",
+            "Geography",
+            "History",
+            "Politics",
+            "Art",
+            "Celebrities",
+            "Animals",
+            "Vehicles",
+            "Comics",
+            "Gadgets",
+            "Anime & Manga",
+            "Cartoons & Animations");
+
         TextField numField = new TextField();
 
         // containers for the above elements
+        HBox titleBox = new HBox(8, title);
         HBox difficultyBox = new HBox(8, difficultyLabel, difficultySelect);
         HBox categoryBox = new HBox(8, categoryLabel, categorySelect);
         HBox numBox = new HBox(8, numLabel, numField);
 
         Button playButton = new Button("Play");
-
+        HBox playBox = new HBox(8, playButton);
         // setup scene
-        root.getChildren().addAll(title, playButton, difficultyBox, categoryBox, numBox);
-        playButton.setAlignment(Pos.CENTER);
+        titleRoot.getChildren().addAll(titleBox, playBox, difficultyBox, categoryBox, numBox);
+        titleBox.setAlignment(Pos.CENTER);
         difficultyBox.setAlignment(Pos.CENTER);
         categoryBox.setAlignment(Pos.CENTER);
         numBox.setAlignment(Pos.CENTER);
-        titleScreen = new Scene(root);
-
-        // setup stage
-        stage.setTitle("Trivia App: Title Screen");
-        stage.setScene(titleScreen);
-        stage.setOnCloseRequest(event -> Platform.exit());
-        stage.sizeToScene();
-        stage.show();
-
-    } // start
+        playBox.setAlignment(Pos.CENTER);
+        titleScreen = new Scene(titleRoot, 800, 320);
+    } // createTitleScreen
 
 } // ApiApp
